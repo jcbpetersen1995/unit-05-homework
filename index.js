f094e28c737901a675e0d42913f03e7459a32ff0
Jacob Petersen <jcb.petersen1995@gmail.com>
2020-01-21 19:32:48 -0700

    Add files via upload
    Finally got time blocks to change color according to time...

--- index.js
+++ index.js
@@ -32,30 +32,100 @@ $(".saveBtn").on("click", function() {
 
   function addColors() {
     var d = new Date();
-    var n = d.getUTCHours() +1;
-    if (n === 9 || n === 10 || n === 11 || n === 12 || n === 13 || n === 14 || n === 15 || n === 16 || n === 17) {
-        $(".time-block9am").addClass("present").removeClass("past, future");
-        $(".time-block10am").addClass("present").removeClass("past, future");
-        $(".time-block11am").addClass("present").removeClass("past, future");
-        $(".time-block12pm").addClass("present").removeClass("past, future");
-        $(".time-block1pm").addClass("present").removeClass("past, future");
-        $(".time-block2pm").addClass("present").removeClass("past, future");
-        $(".time-block3pm").addClass("present").removeClass("past, future");
-        $(".time-block4pm").addClass("present").removeClass("past, future");
-        $(".time-block5pm").addClass("present").removeClass("past, future");
-    } 
-      else {
-        $(".time-block9am").addClass("past").removeClass("present, future");
-        $(".time-block10am").addClass("past").removeClass("present, future");
-        $(".time-block11am").addClass("past").removeClass("present, future");
-        $(".time-block12pm").addClass("past").removeClass("present, future");
-        $(".time-block1pm").addClass("past").removeClass("present, future");
-        $(".time-block2pm").addClass("past").removeClass("present, future");
-        $(".time-block3pm").addClass("past").removeClass("present, future");
-        $(".time-block4pm").addClass("past").removeClass("present, future");
-        $(".time-block5pm").addClass("past").removeClass("present, future");
-    };
-}
+    var n = d.getHours();
+
+        if (n > 9) {
+            $(".time-block9am").addClass("past").removeClass("future, present");
+            }
+        else if (n < 9) {
+            $(".time-block9am").addClass("future").removeClass("past, present");
+        }
+        else {
+            $(".time-block9am").addClass("present").removeClass("past, future");
+        }
+
+        if (n > 10) {
+            $(".time-block10am").addClass("past").removeClass("future, present");
+            }
+        else if (n < 10) {
+            $(".time-block10am").addClass("future").removeClass("past, present");
+        }
+        else {
+            $(".time-block10am").addClass("present").removeClass("past, future");
+        }
+
+        if (n > 11) {
+            $(".time-block11am").addClass("past").removeClass("future, present");
+            }
+        else if (n < 11) {
+            $(".time-block11am").addClass("future").removeClass("past, present");
+        }
+        else {
+            $(".time-block11am").addClass("present").removeClass("past, future");
+        }
+
+        if (n > 12) {
+            $(".time-block12pm").addClass("past").removeClass("future, present");
+            }
+        else if (n < 12) {
+            $(".time-block12pm").addClass("future").removeClass("past, present");
+        }
+        else {
+            $(".time-block12pm").addClass("present").removeClass("past, future");
+        }
+
+        if (n > 13) {
+            $(".time-block1pm").addClass("past").removeClass("future, present");
+            }
+        else if (n < 13) {
+            $(".time-block1pm").addClass("future").removeClass("past, present");
+        }
+        else {
+            $(".time-block1pm").addClass("present").removeClass("past, future");
+        }
+
+        if (n > 14) {
+            $(".time-block2pm").addClass("past").removeClass("future, present");
+            }
+        else if (n < 14) {
+            $(".time-block2pm").addClass("future").removeClass("past, present");
+        }
+        else {
+            $(".time-block2pm").addClass("present").removeClass("past, future");
+        }
+
+        if (n > 15) {
+            $(".time-block3pm").addClass("past").removeClass("future, present");
+            }
+        else if (n < 15) {
+            $(".time-block3pm").addClass("future").removeClass("past, present");
+        }
+        else {
+            $(".time-block3pm").addClass("present").removeClass("past, future");
+        }
+
+        if (n > 16) {
+            $(".time-block4pm").addClass("past").removeClass("future, present");
+            }
+        else if (n < 16) {
+            $(".time-block4pm").addClass("future").removeClass("past, present");
+        }
+        else {
+            $(".time-block4pm").addClass("present").removeClass("past, future");
+        }
+
+        if (n > 17) {
+            $(".time-block5pm").addClass("past").removeClass("future, present");
+            }
+        else if (n < 17) {
+            $(".time-block5pm").addClass("future").removeClass("past, present");
+        }
+        else {
+            $(".time-block5pm").addClass("present").removeClass("past, future");
+        };
+  } 
+
+    
 $(document).ready(function () {
     addColors();
 });
